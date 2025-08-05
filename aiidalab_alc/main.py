d29188e09ef43ba18335b793d4526cfc72ed355e
Benjamin Speake <benjaminspeake@gmail.com>
2025-08-05 12:19:46 +0100

    Add global navigation buttons to the start banner

--- aiidalab_alc/main.py
+++ aiidalab_alc/main.py
@@ -38,42 +38,7 @@ class MainAppView(ipw.VBox):
             """
         )
 
-        self.newCalcLink = ipw.Button(
-            description="New Calculation", 
-            disabled=False, 
-            button_style="primary", 
-            tooltip="Start a new calculation", 
-            icon="plus",
-        )
-        self.newCalcLink.on_click(partial(openLinkInNewTab, "main.ipynb"))
-
-        self.setupResourcesLink = ipw.Button(
-            description="Setup Resources", 
-            disabled=False, 
-            button_style="primary",
-            tooltip="Configure Computational Resources",
-            icon="cogs",
-            # on_click=partial(onLinkClick, getAppDir() / "../home/code_setup.ipynb"),
-        )
-        self.setupResourcesLink.on_click(partial(openLinkInNewTab, "../home/code_setup.ipynb"))
-
-        self.docsLink = ipw.Button(
-            description="Documentation", 
-            disabled=False, 
-            button_style="info", 
-            tooltip="Open Documentation", 
-            icon="book",
-        )
-        self.docsLink.on_click(partial(openLinkInNewTab, "https://github.com/stfc/alc-ux"))
-
-        quickAccess = ipw.HBox(
-            children=[
-                self.newCalcLink,
-                self.setupResourcesLink,
-                self.docsLink,
-            ],
-            layout={'margin': 'auto'}
-        )
+        quickAccess = QuickAccessButtons()
 
         header = ipw.VBox(
             children=[
@@ -148,3 +113,50 @@ class WizardWidget(ipw.VBox):
         return 
 
     
+class QuickAccessButtons(ipw.HBox):
+    def __init__(self, **kwargs):
+        self.newCalcLink = ipw.Button(
+            description="New Calculation", 
+            disabled=False, 
+            button_style="success", 
+            tooltip="Start a new calculation", 
+            icon="plus",
+        )
+        self.newCalcLink.on_click(partial(openLinkInNewTab, "main.ipynb"))
+
+        self.historyLink = ipw.Button(
+            description="History",
+            disabled=False,
+            button_style="primary",
+            tooltip="View Calculation History",
+            icon="history",
+        )
+        self.historyLink.on_click(partial(openLinkInNewTab, "history.ipynb"))
+
+        self.setupResourcesLink = ipw.Button(
+            description="Setup Resources", 
+            disabled=False, 
+            button_style="primary",
+            tooltip="Configure Computational Resources",
+            icon="cogs",
+            # on_click=partial(onLinkClick, getAppDir() / "../home/code_setup.ipynb"),
+        )
+        self.setupResourcesLink.on_click(partial(openLinkInNewTab, "../home/code_setup.ipynb"))
+
+        self.docsLink = ipw.Button(
+            description="Documentation", 
+            disabled=False, 
+            button_style="info", 
+            tooltip="Open Documentation", 
+            icon="book",
+        )
+        self.docsLink.on_click(partial(openLinkInNewTab, "https://github.com/stfc/alc-ux"))
+
+        children=[
+            self.newCalcLink,
+            self.historyLink,
+            self.setupResourcesLink,
+            self.docsLink,
+        ]
+        super().__init__(children=children, layout={'margin': 'auto'}, **kwargs)
+        return 
