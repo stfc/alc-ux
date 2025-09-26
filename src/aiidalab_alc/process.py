cef66766c8c09135426ea7b71b8d6f79086d0fae
Benjamin Speake <benjaminspeake@gmail.com>
2025-09-26 11:41:26 +0100

    Outline basic structure to submit an AiiDA process from the collected input data

--- src/aiidalab_alc/process.py
+++ src/aiidalab_alc/process.py
@@ -1,6 +1,8 @@
 """Module for handling AiiDA processes."""
 
 import traitlets as tl
+from aiida.engine import submit
+from aiida.orm import Dict, load_code
 
 from aiidalab_alc.resources import ComputationalResourcesModel
 from aiidalab_alc.structure import StructureStepModel
@@ -26,6 +28,7 @@ class MainAppModel(tl.HasTraits):
         """Handle the submission of the AiiDA process."""
         if ChemShellProcess.validate_model(self):
             self.process = ChemShellProcess(self)
+            self.process.submit_process()
             print("Submit model called")
         else:
             print("ERROR: Input Validation Failed")
@@ -45,7 +48,7 @@ class ChemShellProcess:
             The main application model containing all necessary data.
         """
         self.model = model
-        self.process = None
+        self.node = None
         return
 
     @classmethod
@@ -71,3 +74,32 @@ class ChemShellProcess:
             return False
         # Add more validation checks as needed
         return True
+
+    def submit_process(self):
+        """Submit the AiiDA process."""
+        builder = load_code(self.model.resourceModel.code_label).get_builder()
+        builder.structure = self.model.structureModel.structure
+        builder.qm_parameters = Dict(
+            {
+                "theory": self.model.workflowModel.qm_theory,
+                "basis": "cc-pvtz"
+                if self.model.workflowModel.basis_quality
+                else "cc-pvdz",
+                "method": "dft",
+                "functional": "B3LYP",
+            }
+        )
+        builder.mm_parameters = Dict(
+            {
+                "theory": self.model.workflowModel.mm_theory,
+            }
+        )
+        builder.force_field_file = self.model.workflowModel.force_field
+        builder.qmmm_parameters = Dict(
+            {
+                "qm_region": self.model.workflowModel.qm_region,
+                "embedding": "mechanical",
+            }
+        )
+        self.node = submit(builder)
+        return
