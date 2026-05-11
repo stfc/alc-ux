f5daf44ccb53ce913ffcec2e9c43cc26040325f4
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-05-11 15:55:02 +0100

    Fix source path in sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,24 +5,22 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+
+sys.path.insert(0, os.path.abspath("../../plugin_template/src"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ALC AiiDAlab Guides'
-copyright = '2025, Ada Lovelace Center'
-author = 'Dr. Benjamin T. Speake'
+project = "ALC AiiDAlab Guides"
+copyright = "2025, Ada Lovelace Center"
+author = "Dr. Benjamin T. Speake"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon"
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 source_suffix = ".rst"
@@ -31,11 +29,9 @@ source_suffix = ".rst"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'piccolo_theme'
-html_static_path = ['_static']
+html_theme = "piccolo_theme"
+html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 # html_logo = "../../images/alc.svg"
 
-html_theme_options = {
-    "source_url": "https://github.com/stfc/alc-ux"
-}
+html_theme_options = {"source_url": "https://github.com/stfc/alc-ux"}
